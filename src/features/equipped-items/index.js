ad3fb4421709a1817f754c2df40ecbc36061fecc
Andrew Steinheiser <me@iamandrew.io>
2018-09-13 12:57:33 -0700

    loading weapon

--- src/features/equipped-items/index.js
+++ src/features/equipped-items/index.js
@@ -6,12 +6,20 @@ import InventorySlot from './inventory-slot.png';
 import './styles.css';
 
 function EquippedItems(props) {
+  let { weapon } = props.stats.equippedItems;
+
+  let weaponSlot = InventorySlot;
+
+  if(weapon) {
+    // load weapon image into slot
+  }
+
   return (
     <div className='equipped-items-container'>
 
       <div className='flex-row'>
         <div style={{
-            backgroundImage: `url('${InventorySlot}')`,
+            backgroundImage: `url('${weaponSlot}')`,
             backgroundSize: 'contain',
             width: '40px',
             height: '40px'
