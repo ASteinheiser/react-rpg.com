98626d240c10c1e17528290446aa9d9c63d2a26f
Andrew Steinheiser <me@iamandrew.io>
2018-09-13 17:00:48 -0700

    created dialog component

---
+++ src/components/dialog/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import './styles.css';
+
+class Dialog extends Component {
+  render() {
+    return(
+      <div>
+        {'DIALOG'}
+      </div>
+    );
+  }
+}
+
+export default Dialog;
