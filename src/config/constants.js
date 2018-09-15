75c916cdc6f743c46741885efe05bdac00867dbc
Andrew Steinheiser <me@iamandrew.io>
2018-09-15 11:30:59 -0700

    adjust sight radius

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,7 +1,7 @@
 // size of tiles
 export const SPRITE_SIZE = 40;
 // how far the player can 'see' or 'explore'
-export const SIGHT_RADIUS = 4;
+export const SIGHT_RADIUS = 3;
 // map size is set to 800 x 600
 export const MAP_HEIGHT = SPRITE_SIZE * 15;
 export const MAP_WIDTH = SPRITE_SIZE * 20;
