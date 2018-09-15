b4b27e1ee04e0ecccf92d642985aad3f9962963a
Andrew Steinheiser <me@iamandrew.io>
2018-09-15 12:11:51 -0700

    optimize explore tiles

--- src/features/player/explore-tiles.js
+++ src/features/player/explore-tiles.js
@@ -4,6 +4,18 @@ import {
   SIGHT_RADIUS
 } from '../../config/constants';
 
+let x, y;
+const radiusTiles = [];
+// calculate a tile map with desired radius
+// (results in array = [[-4,-4]...[0,0]...[4,4]] )
+for (y = -SIGHT_RADIUS; y <= SIGHT_RADIUS; y++) {
+  for (x = -SIGHT_RADIUS; x <= SIGHT_RADIUS; x++) {
+    if ((x * x) + (y * y) <= (SIGHT_RADIUS * SIGHT_RADIUS)) {
+      radiusTiles.push({x, y});
+    }
+  }
+}
+
 export default function exploreTiles(newPos) {
   let x = newPos[1] / SPRITE_SIZE;
   let y = newPos[0] / SPRITE_SIZE;
@@ -27,25 +39,17 @@ export default function exploreTiles(newPos) {
 }
 
 function getSurroundingTiles(startPos) {
-  // set the 'explore' or 'player viewing' radius
-  let radius = SIGHT_RADIUS;
   let newTiles = [];
-  let x, y;
-  // move out from center for radius length
-  for (y = -radius; y <= radius; y++) {
-    for (x = -radius; x <= radius; x++) {
-      // find each surrounding tile
-      if ((x * x) + (y * y) <= (radius * radius)) {
-        // get radius with player location as offset
-        let offsetX = x + startPos[0];
-        let offsetY = y + startPos[1];
-        // if it is inside the bounds
-        if(offsetX >= 0 && offsetX < 15 && offsetY >= 0 && offsetY < 20) {
-          // explore this tile...
-          newTiles.push([offsetX, offsetY]);
-        }
-      }
+  // add player position as offset to each radius tile
+  radiusTiles.forEach(({x, y}) => {
+    // get radius with player location as offset
+    let offsetX = x + startPos[0];
+    let offsetY = y + startPos[1];
+    // if it is inside the bounds
+    if(offsetX >= 0 && offsetX < 15 && offsetY >= 0 && offsetY < 20) {
+      // explore this tile...
+      newTiles.push([offsetX, offsetY]);
     }
-  }
+  });
   return newTiles;
 }
