3b0f3cf8b6c48c7503f687c8470d49ebf90795e8
Andrew Steinheiser <me@iamandrew.io>
2018-09-15 08:06:30 -0700

    finished map 1_4 and added 1_5

--- src/data/maps/index.js
+++ src/data/maps/index.js
@@ -1,11 +1,15 @@
 import map1_1 from './1_1';
 import map1_2 from './1_2';
 import map1_3 from './1_3';
+import map1_4 from './1_4';
+import map1_5 from './1_5';
 
 const maps = {
   '1_1': map1_1,
   '1_2': map1_2,
-  '1_3': map1_3
+  '1_3': map1_3,
+  '1_4': map1_4,
+  '1_5': map1_5
 };
 
 export default maps;
