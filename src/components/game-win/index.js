f9bd056d7f51026c8b29d627c6e0d3ab2629f9d4
Andrew Steinheiser <me@iamandrew.io>
2018-09-18 09:33:27 -0700

    game win dialog works

--- src/components/game-win/index.js
+++ src/components/game-win/index.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import { connect }          from 'react-redux';
 
-import Button       from '../button';
-import Dialog       from '../dialog';
-import store        from '../../config/store';
+import Button         from '../button';
+import Dialog         from '../dialog';
+import resetGameState from '../../modules/reset-game-state';
+import store          from '../../config/store';
 
 import './styles.css';
 
@@ -17,13 +18,29 @@ class GameWin extends Component {
   }
 
   render() {
-    const { world, monsters } = this.props;
-    console.log(monsters);
-    console.log(world.currentMap);
-    // case for when player has not won yet
+    const { components } = this.props.monsters;
+    let monstersRemain = false;
+    // check monsters by map
+    Object.keys(components).forEach(mapId => {
+      // see if there are any monsters on any maps
+      monstersRemain = Object.keys(components[mapId]).length > 0;
+    });
+    // player has defeated all enemies!
+    if(!monstersRemain) {
+      return(
+        <Dialog>
+          {'You did it!'}
+          <Button
+            onClick={() => { resetGameState(); }}
+            icon='question'
+            title={'Play Again'} />
+        </Dialog>
+      );
+    }
+    // player hasn't killed all enemies
     return(
       <Dialog>
-        {'victory?'}
+        {'You need to kill all enemies'}
         <Button
           onClick={this.handleCloseDialog.bind(this)}
           icon='reply'
@@ -33,8 +50,8 @@ class GameWin extends Component {
   }
 }
 
-const mapStateToProps = ({ world, monsters }) => {
-  return { world, monsters };
+const mapStateToProps = ({ monsters }) => {
+  return { monsters };
 }
 
 export default connect(mapStateToProps)(GameWin);
