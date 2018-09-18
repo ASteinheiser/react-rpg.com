3f471655c0ac08ea97e3c4d70e7e1a8734d1a2a0
Andrew Steinheiser <me@iamandrew.io>
2018-09-18 16:30:57 -0700

    added to shop dialog

--- src/components/shop-dialog/index.js
+++ src/components/shop-dialog/index.js
@@ -19,24 +19,29 @@ export default class ShopDialog extends Component {
   render() {
     return(
       <Dialog>
-        <div className='flex-row shop-container'>
-          <div className='flex-column shop-container-child'>
-            <div className='shop-keep-animated'
-              style={{backgroundImage: `url(${ShopKeepSprite})`}}/>
-          </div>
+        <div className='flex-column'>
 
-          <div className='flex-column shop-container-child'>
-            <div>
-              {'Shop inventory!!'}
+          <span className='shop-title-text'>{'Shop'}</span>
+
+          <div className='flex-row shop-container'>
+            <div className='flex-column shop-container-child'>
+              <div className='shop-keep-animated'
+                style={{backgroundImage: `url(${ShopKeepSprite})`}}/>
+            </div>
+
+            <div className='flex-column shop-container-child'>
+              <div>
+                {'Shop inventory!!'}
+              </div>
             </div>
           </div>
-        </div>
 
-        <div className='shop-button-container'>
-          <Button
-            onClick={this.handleCloseDialog.bind(this)}
-            icon='walking'
-            title={'Leave Shop'} />
+          <div className='shop-button-container'>
+            <Button
+              onClick={this.handleCloseDialog.bind(this)}
+              icon='walking'
+              title={'Leave Shop'} />
+          </div>
         </div>
       </Dialog>
     );
