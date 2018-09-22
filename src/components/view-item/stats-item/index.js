f8f7c2cbbdb69435a0b5ecfbffbd2293ebb78306
Andrew Steinheiser <me@iamandrew.io>
2018-09-21 23:37:20 -0700

    add colored effect text on stats item

--- src/components/view-item/stats-item/index.js
+++ src/components/view-item/stats-item/index.js
@@ -3,6 +3,21 @@ import React, { Component } from 'react';
 import './styles.css';
 
 class StatsItem extends Component {
+
+  getColor(name) {
+    switch(name) {
+      case 'damage':
+        return 'light-red';
+      case 'defence':
+        return 'purple';
+      case 'hp':
+        return 'green';
+      case 'VS. dragon':
+        return 'orange';
+      default:
+    }
+  }
+
   render() {
     const { stats } = this.props;
 
@@ -11,7 +26,7 @@ class StatsItem extends Component {
         <span>
           { stats.name }
         </span>
-        <span>
+        <span style={{ color: `var(--${this.getColor(stats.name)})` }}>
           { '+' + stats.value }
         </span>
       </div>
