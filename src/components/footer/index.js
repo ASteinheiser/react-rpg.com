92c31bc942675fddfd6a0d3e72f84adf40d49988
Andrew Steinheiser <me@iamandrew.io>
2019-01-08 00:05:59 -0700

    show version in footer

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,16 +1,22 @@
 import React from 'react';
 
+import packageJson from '../../../package.json';
+
 import './styles.css';
 
 const Footer = (props) => {
   return(
     <div className='footer-container'>
-      <span>{'Made with ♥ by Andrew Steinheiser - '}</span>
+      <span>
+        {`Made with ♥ by Andrew Steinheiser - v${packageJson.version} - `}
+      </span>
+
       <a href='https://github.com/ASteinheiser/react-rpg.com'
         target='_blank'
         rel='noopener noreferrer'>
         {'View Source'}
       </a>
+
     </div>
   );
 }
