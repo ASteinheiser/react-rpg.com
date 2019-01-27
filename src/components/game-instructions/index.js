130f01271896391badefcdc3b1775c14bb0e3054
Andrew Steinheiser <me@iamandrew.io>
2019-01-27 15:07:57 -0700

    added icons for movement with arrow and wasd keys to game instructions page

--- src/components/game-instructions/index.js
+++ src/components/game-instructions/index.js
@@ -3,6 +3,10 @@ import React from 'react';
 import Button from '../button';
 import Dialog from '../dialog';
 
+import ArrowKeys from './assets/arrow-keys.png';
+// import Swipe     from './assets/swipe.png';
+import WASDKeys  from './assets/wasd-keys.png';
+
 import './styles.scss';
 
 const GameInstructions = (props) => {
@@ -16,10 +20,26 @@ const GameInstructions = (props) => {
       </div>
 
       <div className='game-instructions-text'>
-        {`ATTACK >> Double-tap / 'Space' / 'Enter'`}
-        <br />
-        <br />
-        {`MOVE >> Swipe / 'WASD' / Arrow Keys`}
+        <span>
+          {`MOVEMENT`}
+        </span>
+
+        <div className='flex-row space-between'>
+          {/* <img src={Swipe} alt='swipe' /> */}
+          <img src={ArrowKeys} alt='arrow-keys' />
+          <img src={WASDKeys} alt='wasd-keys' />
+        </div>
+
+        <span style={{paddingTop: 16}}>
+          {`ATTACK`}
+        </span>
+
+        <div className='flex-row space-between'>
+          {/* <img src={ArrowKeys} alt='arrow-keys' />
+          <img src={ArrowKeys} alt='arrow-keys' />
+          <img src={ArrowKeys} alt='arrow-keys' /> */}
+        </div>
+
       </div>
 
       <div className='flex-column game-start-button'>
