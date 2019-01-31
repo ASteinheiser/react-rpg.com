93785e513b78df74af1dd04927f396ec2daf8bc0
Andrew Steinheiser <me@iamandrew.io>
2019-01-31 09:24:37 -0700

    render map padding tiles to have boundary fog

--- src/features/map/map-padding.js
+++ src/features/map/map-padding.js
@@ -9,8 +9,29 @@ let variation = [];
 // generate static variations for the map padding tiles
 // to avoid setting it during render and having the tiles
 // variation swap with each player move
-for(let i = 0; i < 125; i ++) {
+for(let i = 0; i < 25; i ++) {
   variation.push(Math.round(Math.random() * (4 - 1) + 1));
+}
+
+function BoundaryTile(props) {
+  const { tile, variation } = props;
+
+  return(
+    <div style={{
+      backgroundImage: `url(/tiles/${getTileSprite(tile, variation)}.png)`,
+      display: 'inline-flex',
+      height: SPRITE_SIZE,
+      width: SPRITE_SIZE
+    }}>
+      <div style={{
+        backgroundColor: '#000',
+        opacity: '0.2',
+        display: 'inline-flex',
+        height: SPRITE_SIZE,
+        width: SPRITE_SIZE
+      }} />
+    </div>
+  );
 }
 
 export default function MapPadding(props) {
@@ -21,16 +42,14 @@ export default function MapPadding(props) {
     let Row = [];
     for(let j = 0; j < 20; j ++) {
       Row.push(
-        <div style={{
-          backgroundImage: `url(/tiles/${getTileSprite(tile, variation[i + j])}.png)`,
-          display: 'inline-flex',
-          height: SPRITE_SIZE,
-          width: SPRITE_SIZE
-        }} />
+        <BoundaryTile
+          tile={tile}
+          variation={variation[i + j]}
+          key={`padding-top-${i + j}`} />
       );
     }
     TilesUpDown.push(
-      <div className='row' style={{ height: SPRITE_SIZE }}>
+      <div className='row' style={{ height: SPRITE_SIZE }} key={`row-${i}`}>
         { Row }
       </div>
     );
@@ -41,16 +60,14 @@ export default function MapPadding(props) {
     let Row = [];
     for(let j = 0; j < 5; j ++) {
       Row.push(
-        <div style={{
-          backgroundImage: `url(/tiles/${getTileSprite(tile, variation[i + j])}.png)`,
-          display: 'inline-flex',
-          height: SPRITE_SIZE,
-          width: SPRITE_SIZE
-        }} />
+        <BoundaryTile
+          tile={tile}
+          variation={variation[i + j]}
+          key={`padding-side-${i + j}`} />
       );
     }
     TilesSide.push(
-      <div className='row' style={{ height: SPRITE_SIZE }}>
+      <div className='row' style={{ height: SPRITE_SIZE }} key={`row-${i}`}>
         { Row }
       </div>
     );
