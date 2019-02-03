33fd2963e5bead245ed3f8a95383c0aa1f0dd34d
Andrew Steinheiser <me@iamandrew.io>
2019-02-03 13:10:11 -0700

    add buttons for ios and android store apps

--- src/components/download-app-popup/index.js
+++ src/components/download-app-popup/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 
 import store from '../../config/store';
 
+import iosStore     from './ios-store.png';
+import androidStore from './android-store.png';
+
 import './styles.scss';
 
-// - [ ] android https://play.google.com/store/apps/details?id=com.reactrpgnative
-// - [ ] ios https://itunes.apple.com/WebObjects/MZStore.woa/wa/viewSoftware?id=1450907766&mt=8
+const ANDROID_URL = 'https://play.google.com/store/apps/details?id=com.reactrpgnative';
+const IOS_URL = 'https://itunes.apple.com/WebObjects/MZStore.woa/wa/viewSoftware?id=1450907766&mt=8';
 
 const DownloadAppPopup = (props) => {
 
@@ -26,8 +29,16 @@ const DownloadAppPopup = (props) => {
 
       <div className='download-app-box white-border'>
         <span>
-          {'React RPG is also available on mobile!'}
+          {'React RPG is now on iOS and Android!'}
         </span>
+
+        <a href={IOS_URL} target='_blank' rel="noopener noreferrer">
+          <img className='ios-store-icon' src={iosStore} alt='ios-store' />
+        </a>
+
+        <a href={ANDROID_URL} target='_blank' rel="noopener noreferrer">
+          <img className='android-store-icon' src={androidStore} alt='android-store' />
+        </a>
 
         <div className='flex-row space-between'>
           <div className='close-option' onClick={handleOptOut}>
