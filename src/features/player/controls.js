b4f71a39dd437344030f0b4b1a8fac206c07e684
Andrew Steinheiser <me@iamandrew.io>
2019-02-08 01:34:02 -0700

    touch swipe faster move rate, matches keyboard press down rate

--- src/features/player/controls.js
+++ src/features/player/controls.js
@@ -105,7 +105,7 @@ export default function Controls(player) {
         default:
           // console.log(`Unmapped pan direction ${direction}`);
       }
-    }, ANIMATION_SPEED * 1.35);
+    }, ANIMATION_SPEED * 1.25);
   },
     ANIMATION_SPEED,
     { maxWait: ANIMATION_SPEED, leading: true, trailing: false }
