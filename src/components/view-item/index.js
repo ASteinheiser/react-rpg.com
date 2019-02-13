be08b77b713528ecbd79ab52937864c372495533
Andrew Steinheiser <me@iamandrew.io>
2019-02-13 10:34:59 -0700

    move sell and buy item to their own functions

--- src/components/view-item/index.js
+++ src/components/view-item/index.js
@@ -9,141 +9,44 @@ import { uuidv4 }    from '../../modules/uuid-v4';
 
 import StatsItem     from './stats-item';
 import consumePotion from './consume-potion';
+import buyItem       from './buy-item';
 import dropItem      from './drop-item';
 import equipItem     from './equip-item';
 import unequipItem   from './unequip-item';
-
-import store from '../../config/store';
+import sellItem      from './sell-item';
 
 import './styles.scss';
 
-const ViewItem = ({ sell, buy, onClose, data, stats, inventory, unequipItem,
-                    equipItem, dropItem, consumePotion }) => {
+const ViewItem = ({ sell, buy, onClose, data, stats, unequipItem, buyItem,
+                    equipItem, dropItem, consumePotion, sellItem }) => {
 
   const [confirmPotion, setConfirmPotion] = useState(false);
   const [confirmDrop, setConfirmDrop] = useState(false);
   const [confirmSell, setConfirmSell] = useState(false);
   const [confirmBuy, setConfirmBuy] = useState(false);
 
-  function handleConfirmBuy(item) {
-    const { gold } = stats;
-    const { items, maxItems } = inventory;
-    // make sure player has enough gold
-    if(gold >= item.value) {
-      // if it's a backpack upgrade
-      if(item.type === 'upgrade::backpack') {
-        store.dispatch({
-          type: 'LOSE_GOLD',
-          payload: { value: item.value }
-        });
-        store.dispatch({
-          type: 'UPGRADE_PACK',
-          payload: { slots: item.slots }
-        });
-      } // otherwise, see if there's room in the inventory
-      else if(items.length < maxItems) {
-        store.dispatch({
-          type: 'LOSE_GOLD',
-          payload: { value: item.value }
-        });
-        store.dispatch({
-          type: 'GET_ITEM',
-          payload: item
-        });
-      } else {
-        // inventory full
-        store.dispatch({
-          type: 'TOO_MANY_ITEMS',
-          payload: item
-        });
-      }
-    } else {
-      // not enough gold!
-      store.dispatch({
-        type: 'NOT_ENOUGH_GOLD',
-        payload: item
-      });
-    }
-
-    onClose();
-  }
-
-  function handleConfirmSell(item, sellPrice) {
-    store.dispatch({
-      type: 'GET_GOLD',
-      payload: { value: sellPrice }
-    });
-    store.dispatch({
-      type: 'DROP_ITEM',
-      payload: item
-    });
-
-    const { equippedItems } = store.getState().stats;
-    let itemEquipped = false;
-    // check if the item was equipped, then take it off
-    switch(item.type) {
-      case 'weapon':
-        if(JSON.stringify(equippedItems['weapon']) === JSON.stringify(item)) itemEquipped = true;
-        break;
-      case 'ring':
-        if(JSON.stringify(equippedItems['ring']) === JSON.stringify(item)) itemEquipped = true;
-        break;
-      case 'armor::body':
-        if(equippedItems['armor'] && JSON.stringify(equippedItems['armor']['body']) === JSON.stringify(item)) itemEquipped = true;
-        break;
-      case 'armor::pants':
-        if(equippedItems['armor'] && JSON.stringify(equippedItems['armor']['pants']) === JSON.stringify(item)) itemEquipped = true;
-        break;
-      case 'armor::helmet':
-        if(equippedItems['armor'] && JSON.stringify(equippedItems['armor']['helmet']) === JSON.stringify(item)) itemEquipped = true;
-        break;
-      case 'armor::boots':
-        if(equippedItems['armor'] && JSON.stringify(equippedItems['armor']['boots']) === JSON.stringify(item)) itemEquipped = true;
-        break;
-      case 'armor::gloves':
-        if(equippedItems['armor'] && JSON.stringify(equippedItems['armor']['gloves']) === JSON.stringify(item)) itemEquipped = true;
-        break;
-      default:
-    }
-
-    if(itemEquipped) {
-      store.dispatch({
-        type: 'UNEQUIP_ITEM',
-        payload: { data: item }
-      });
-    }
-
-    onClose();
-  }
-
-  const equipped = stats.equippedItems;
-
   let itemStats = [];
-
   let itemIsEquipped = false;
+  const equipped = stats.equippedItems;
 
   // find the type of item
   switch(data.type) {
 
     case 'upgrade::backpack':
-      // display stats
       itemStats.push(<StatsItem stats={{ name: 'slots', value: data.slots }} key={uuidv4()} />);
       break;
 
     case 'potion':
-      // display stats
       itemStats.push(<StatsItem stats={{ name: 'heal', value: Math.ceil((data.hp / stats.maxHp) * 100) }} key={uuidv4()} />);
       break;
 
     case 'weapon':
       itemIsEquipped = (JSON.stringify(equipped['weapon']) === JSON.stringify(data));
-      // display stats
       itemStats.push(<StatsItem stats={{ name: 'damage', value: data.damage }} key={uuidv4()} />);
       // if there's a bonus
       if(data.bonus) {
         let bonusType = data.bonus.split('::')[0];
         let bonusMult = parseFloat(data.bonus.split('::')[1], 10);
-        // display the bonus
         itemStats.push(<StatsItem stats={{ name: `VS. ${bonusType}`, value: bonusMult + 'x' }} key={uuidv4()} />);
       }
       break;
@@ -157,44 +60,32 @@ const ViewItem = ({ sell, buy, onClose, data, stats, inventory, unequipItem,
       break;
 
     case 'armor::helmet':
-      // properly check the armor
       itemIsEquipped = (equipped['armor'] && JSON.stringify(equipped['armor']['helmet']) === JSON.stringify(data));
-      // display stats
       itemStats.push(<StatsItem stats={{ name: 'defence', value: data.defence }} key={uuidv4()} />);
       break;
 
     case 'armor::body':
-      // properly check the armor
       itemIsEquipped = (equipped['armor'] && JSON.stringify(equipped['armor']['body']) === JSON.stringify(data));
-      // display stats
       itemStats.push(<StatsItem stats={{ name: 'defence', value: data.defence }} key={uuidv4()} />);
       break;
 
     case 'armor::gloves':
-      // properly check the armor
       itemIsEquipped = (equipped['armor'] && JSON.stringify(equipped['armor']['gloves']) === JSON.stringify(data));
-      // display stats
       itemStats.push(<StatsItem stats={{ name: 'defence', value: data.defence }} key={uuidv4()} />);
       break;
 
     case 'armor::boots':
-      // properly check the armor
       itemIsEquipped = (equipped['armor'] && JSON.stringify(equipped['armor']['boots']) === JSON.stringify(data));
-      // display stats
       itemStats.push(<StatsItem stats={{ name: 'defence', value: data.defence }} key={uuidv4()} />);
       break;
 
     case 'armor::pants':
-      // properly check the armor
       itemIsEquipped = (equipped['armor'] && JSON.stringify(equipped['armor']['pants']) === JSON.stringify(data));
-      // display stats
       itemStats.push(<StatsItem stats={{ name: 'defence', value: data.defence }} key={uuidv4()} />);
       break;
 
     default:
   }
-
-  const itemSellPrice = Math.ceil(data.value / 2);
 
   return(
     <MicroDialog onClose={onClose}>
@@ -292,11 +183,14 @@ const ViewItem = ({ sell, buy, onClose, data, stats, inventory, unequipItem,
       {
         confirmSell &&
           <ConfirmDialog
-            text={`Are you sure you want to sell your ${data.name} for ${itemSellPrice} gold ?`}
+            text={`Are you sure you want to sell your ${data.name} for ${Math.ceil(data.value / 2)} gold ?`}
             cancelText={'Cancel'}
             acceptText={'Sell'}
             acceptIcon={'coins'}
-            confirm={() => handleConfirmSell(data, itemSellPrice)}
+            confirm={() => {
+              sellItem(data);
+              onClose();
+            }}
             onClose={() => setConfirmSell(false)} />
       }
       {
@@ -306,7 +200,10 @@ const ViewItem = ({ sell, buy, onClose, data, stats, inventory, unequipItem,
             cancelText={'Cancel'}
             acceptText={'Buy'}
             acceptIcon={'coins'}
-            confirm={() => handleConfirmBuy(data)}
+            confirm={() => {
+              buyItem(data);
+              onClose();
+            }}
             onClose={() => setConfirmBuy(false)} />
       }
       {
@@ -326,8 +223,8 @@ const ViewItem = ({ sell, buy, onClose, data, stats, inventory, unequipItem,
   );
 }
 
-const mapStateToProps = ({ inventory, stats }) => ({ inventory, stats });
+const mapStateToProps = ({ stats }) => ({ stats });
 
-const actions = { consumePotion, dropItem, equipItem, unequipItem };
+const actions = { buyItem, consumePotion, dropItem, equipItem, unequipItem, sellItem };
 
 export default connect(mapStateToProps, actions)(ViewItem);
