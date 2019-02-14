6f88e8f2ff57a46974b10bbd92d588ba6b48da8d
Andrew Steinheiser <me@iamandrew.io>
2019-02-13 21:58:07 -0700

    fix hp bar initial animation

--- src/components/health-bar/index.js
+++ src/components/health-bar/index.js
@@ -4,11 +4,15 @@ import './styles.scss';
 
 const HealthBar = ({ value, max }) => {
   // hide hp bars on full health units
-  if(value === max) return null;
+  const fullHp = (value === max);
 
   return(
     <span className='flex-row'>
-      <span className='health-bar__container'>
+      <span className='health-bar__container'
+        style={{
+          width: fullHp ? 0 : 38,
+          border: fullHp ? 'none' : '1px solid var(--green)'
+        }}>
 
         <span className='health-bar__value'
           style={{ width: `${(value / max) * 100}%` }} />
