815a6953832c92bd34c8243eae0464fa79b2c8ec
Andrew Steinheiser <me@iamandrew.io>
2019-02-18 11:54:31 -0700

    fixed redux devtool function inside compose

--- src/config/store.js
+++ src/config/store.js
@@ -38,7 +38,7 @@ const store = createStore(
   persistedReducer,
   compose(
     applyMiddleware(thunk),
-    process.env.NODE_ENV === 'development' && window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    process.env.NODE_ENV === 'development' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__() || compose
   )
 );
 
