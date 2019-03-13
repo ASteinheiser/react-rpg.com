11d13925fbf866cf5e542b30963ed325515ee7ec
Kaji Tetsushi <kajitetsushi@outlook.com>
2019-03-14 02:15:33 +0800

    refactor(inventory): delegate window keydown event listeners to a button class
    The previous commit [d4d81ef] stops the inventory from being visible when it's not supposed to (e.g. during game menu / story dialog / etc.).
    However, the event listeners are tied to the inventory, which doesn't unmount when the game is paused (inventory "disabled" prop).
    
    The button is mounted & unmounted using that exact same prop.
    Thus, it can be used to manage the keydown event listener, assuming the button had its own component class.

--- src/features/inventory/index.js
+++ src/features/inventory/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect }          from 'react-redux';
 
-import Button          from '../../components/button';
+import InventoryButton from './button';
 import toggleInventory from '../dialog-manager/actions/toggle-inventory';
 
 import './styles.scss';
@@ -13,16 +13,6 @@ class Inventory extends Component {
     this.state = {
       newItemIndicator: false
     };
-  }
-
-  componentDidMount() {
-    // Attach this to listen for the inventory visibility toggle key.
-    window.addEventListener('keydown', this.handleKeyDown);
-  }
-
-  componentWillUnmount() {
-    // Clean up the event listener when this component unmounts.
-    window.removeEventListener('keydown', this.handleKeyDown);
   }
 
   componentDidUpdate(prevProps, prevState) {
@@ -52,8 +42,6 @@ class Inventory extends Component {
         // because right now, they're being used for the attack order.
         break;
       case 73:
-        // Don't trigger the inventory when the button is hidden.
-        if (this.props.disabled) return; // Inventory hasn't been unmounted yet!
         // Don't trigger any of the other active keydown listeners.
         event.stopPropagation();
         // Toggle inventory with the "I" key.
@@ -81,7 +69,7 @@ class Inventory extends Component {
       <div className='flex-row inventory__container'>
         {
           !disabled &&
-            <Button
+            <InventoryButton
               small={sideMenu}
               indicator={newItemIndicator}
               onClick={this._toggleInventory.bind(this)}
