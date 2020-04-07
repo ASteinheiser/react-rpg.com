3de259af6736d35bbdd445496c8dcc2be3a5b3ce
Josh Hill <josh.hill189@gmail.com>
2020-04-07 17:02:28 +1200

    Every 5 Levels Character Gains Points

--- src/utils/is-ability-allocation-level.js
+++ src/utils/is-ability-allocation-level.js
@@ -1,4 +1,4 @@
 // Check if the level the player is at provides them with more ability points.
 export default function isAbilityAllocationLevel(level) {
-    return level % 5 === 1;
+    return level % 5 === 0;
 }
