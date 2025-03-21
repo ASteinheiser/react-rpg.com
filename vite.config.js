3c4f01993be661421fbc152145f376f9a0494ed8
ASteinheiser <me@iamandrew.io>
2025-03-21 12:08:58 -0700

    update package, vite config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: '/roll-for-reaction/',
+    base: '/',
     optimizeDeps: {
         esbuildOptions: {
             target: 'esnext',
