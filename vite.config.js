629ab8a5e0536e36b27d72ee822af613244d34f9
ASteinheiser <me@iamandrew.io>
2025-03-21 19:36:22 -0700

    update vite base

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/',
+    base: '/react-rpg.com',
     optimizeDeps: {
         esbuildOptions: {
             target: 'esnext',
