105f1a5a342ffa37b41de5f7834ff5ae938d96de
ASteinheiser <me@iamandrew.io>
2025-03-21 18:48:36 -0700

    add manifest to vite config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
     base: '/',
     optimizeDeps: {
         esbuildOptions: {
@@ -13,4 +13,40 @@ export default defineConfig({
     build: {
         target: 'esnext',
     },
+    plugins: [
+        react(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+            manifest: {
+                name: 'React RPG: 2e',
+                short_name: 'React RPG: 2e',
+                theme_color: '#0f0f0f',
+                icons: [
+                    {
+                        src: 'pwa-64x64.png',
+                        sizes: '64x64',
+                        type: 'image/png'
+                    },
+                    {
+                        src: 'pwa-192x192.png',
+                        sizes: '192x192',
+                        type: 'image/png'
+                    },
+                    {
+                        src: 'pwa-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                        purpose: 'any'
+                    },
+                    {
+                        src: 'maskable-icon-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                        purpose: 'maskable'
+                    }
+                ],
+            },
+        }),
+    ],
 });
