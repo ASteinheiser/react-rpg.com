9a7f9d51ffd548fdd2bb82cd4517f5b9d70cb5e2
ASteinheiser <me@iamandrew.io>
2025-03-21 21:31:10 -0700

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('https://react-rpg.com/sw.js', { scope: 'https://react-rpg.com/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
